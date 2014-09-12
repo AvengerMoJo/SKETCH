fa20d8db4b409ead74cbeedc570104eda71dfd15
avengermojo <avengermojo@gmail.com>
2014-09-12 10:35:57 +0800

    Fix typo add Encoder with others public key

--- Android/src/sketch/avengergear/com/CipherHelper.java
+++ Android/src/sketch/avengergear/com/CipherHelper.java
@@ -35,33 +35,56 @@ public class CipherHelper
         Security.insertProviderAt(new org.spongycastle.jce.provider.BouncyCastleProvider(), 1);
     }
 
-    Cipher rsa_encoder_chiper = null;
-    Cipher rsa_decoder_chiper = null;
+    Cipher rsa_encoder_cipher = null;
+    Cipher rsa_decoder_cipher = null;
 
     public CipherHelper( PrivateKey private_key, PublicKey public_key )
     { 
         try { 
-            rsa_encoder_chiper = Cipher.getInstance("RSA/None/PKCS1Padding", "SC");
-            rsa_decoder_chiper = Cipher.getInstance("RSA/None/PKCS1Padding", "SC");
+            rsa_encoder_cipher = Cipher.getInstance("RSA/None/PKCS1Padding", "SC");
+            rsa_decoder_cipher = Cipher.getInstance("RSA/None/PKCS1Padding", "SC");
 
-            rsa_encoder_chiper.init(Cipher.ENCRYPT_MODE, public_key );
-            rsa_decoder_chiper.init(Cipher.DECRYPT_MODE, private_key );
+            rsa_encoder_cipher.init(Cipher.ENCRYPT_MODE, public_key );
+            rsa_decoder_cipher.init(Cipher.DECRYPT_MODE, private_key );
         } catch (InvalidKeyException e) {
             Log.e(TAG, "InvalidKey exception", e);
+        } catch (NoSuchAlgorithmException e ){
+            Log.e(TAG, "No Algorithum exception", e);
         } catch (NoSuchPaddingException e) {
             Log.e(TAG, "No Padding exception", e);
         } catch (NoSuchProviderException e) {
             Log.e(TAG, "No Provider exception", e);
+        } 
+    }
+
+    public byte[] encodeBytesWithPublicKey(byte[] info, PublicKey public_key)
+    {
+        byte[] answer = null;
+        try {
+            Cipher temp_encoder_cipher = Cipher.getInstance("RSA/None/PKCS1Padding", "SC");
+            temp_encoder_cipher.init(Cipher.ENCRYPT_MODE, public_key );
+            answer = rsa_encoder_cipher.doFinal(info);
+        } catch (BadPaddingException e) {
+            Log.e(TAG, "Bad Padding exception", e);
+        } catch (IllegalBlockSizeException e) { 
+            Log.e(TAG, "Illegal Block Size exception", e);
+        } catch (InvalidKeyException e) {
+            Log.e(TAG, "InvalidKey exception", e);
         } catch (NoSuchAlgorithmException e ){
             Log.e(TAG, "No Algorithum exception", e);
-        } 
+        } catch (NoSuchPaddingException e) {
+            Log.e(TAG, "No Padding exception", e);
+        } catch (NoSuchProviderException e) {
+            Log.e(TAG, "No Provider exception", e);
+        }
+        return answer;
     }
 
     public byte[] encodeBytes(byte[] info)
     {
         byte[] answer = null;
         try {
-            answer = rsa_encoder_chiper.doFinal(info);
+            answer = rsa_encoder_cipher.doFinal(info);
         } catch (BadPaddingException e) {
             Log.e(TAG, "Bad Padding exception", e);
         } catch (IllegalBlockSizeException e) { 
@@ -74,7 +97,7 @@ public class CipherHelper
     {
         byte[] answer = null;
         try {
-            answer = rsa_decoder_chiper.doFinal(info);
+            answer = rsa_decoder_cipher.doFinal(info);
         } catch (BadPaddingException e) {
             Log.e(TAG, "Bad Padding exception", e);
         } catch (IllegalBlockSizeException e) { 
